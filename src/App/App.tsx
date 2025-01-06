2a388de2df96fe42c5721b5524f5600975e56fe4
alsushkaa <->
2025-01-06 14:54:15 +0300

    .

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -5,14 +5,12 @@ import { Outlet } from 'react-router-dom';
 
 import styles from './App.module.css';
 import Navbar from '../Components/Navbar';
-import ButtonInstall from '../Components/ButtonInstall';
 
 function App({ className, style }: AppProps) {
 	return (
 		<div className={clsx(styles.root, className)} style={style}>
 			<Outlet />
 			<Navbar />
-			<ButtonInstall />
 		</div>
 	);
 }
