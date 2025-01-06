64c5aeefc6b26c28d832485301ded4914b39432c
alsushkaa <->
2025-01-06 12:16:41 +0300

    .

--- manifest.pwa.ts
+++ manifest.pwa.ts
@@ -15,30 +15,30 @@ const manifest: Partial<VitePWAOptions> = {
 		background_color: '#f4f4f4',
 		icons: [
 			{
-				src: 'icons/144_144.png',
+				src: './icons/144_144.png',
 				type: 'image/png',
 				sizes: '144x144',
 			},
 			{
-				src: 'icons/192_192_maskable.png',
+				src: './icons/192_192_maskable.png',
 				type: 'image/png',
 				sizes: '192x192',
 				purpose: 'maskable',
 			},
 			{
-				src: 'icons/384_384_maskable.png',
+				src: './icons/384_384_maskable.png',
 				type: 'image/png',
 				sizes: '384x384',
 				purpose: 'maskable',
 			},
 			{
-				src: 'icons/512_512_maskable.png',
+				src: './icons/512_512_maskable.png',
 				type: 'image/png',
 				sizes: '512x512',
 				purpose: 'maskable',
 			},
 			{
-				src: 'icons/1024_1024_maskable.png',
+				src: './icons/1024_1024_maskable.png',
 				type: 'image/png',
 				sizes: '1024x1024',
 				purpose: 'maskable',
